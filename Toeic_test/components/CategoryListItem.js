7c9a66a019f836c41f9bf0605f632d50de68a521
phuctd99 <thangtran990201@gmail.com>
2020-03-19 20:17:40 +0700

    Run HelloWorld by react-native

---
+++ Toeic_test/components/CategoryListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import{
+    Text,
+    View,
+} from 'react-native'
+
+export default function CategoryListItem(props){
+    return <View>
+        <Text>Exam 1</Text>
+    </View>
+}
