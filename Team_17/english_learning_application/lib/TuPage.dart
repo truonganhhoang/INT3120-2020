84d9abd9a8d19421b9a16318001d2117ebc8d080
hung06031998 <hungnv6398@gmail.com>
2020-03-22 21:44:49 +0700

    create simple splash screen

---
+++ Team_17/english_learning_application/lib/TuPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              child: ListTile(
+                  title: Text(index.toString()),
+                  trailing: Icon(Icons.favorite_border, color: Colors.green),
+                  subtitle: Text("Đây là subtitle của " + index.toString()),
+                  onTap: () {}));
+        },
+      ),
+    );
+  }
+}
