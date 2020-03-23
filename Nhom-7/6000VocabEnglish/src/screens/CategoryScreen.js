f79ffb29f19fec7353118e5ee635e910b2f23e28
VuVanHoc <hocvanvu1999@gmail.com>
2020-03-23 10:57:08 +0700

    fix bug

--- Nhom-7/6000VocabEnglish/src/screens/CategoryScreen.js
+++ Nhom-7/6000VocabEnglish/src/screens/CategoryScreen.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
 const GLOBAL = require("../utils/Globals");
 import { Text, View, StyleSheet, FlatList, ScrollView } from "react-native";
 import OverviewTopicItem from "../components/OverviewTopicItem";
@@ -7,19 +6,18 @@ import OverviewTopicItem from "../components/OverviewTopicItem";
 export default class CategoryScreen extends Component {
   static navigationOptions = ({ route }) => {
     return {
-
       title: route.params.categoryTitle,
       headerTitleStyle: {
         color: GLOBAL.COLOR.ORANGE,
         fontWeight: "bold"
       },
-      headerTitleAlign: "center",
+      headerTitleAlign: "center"
       //title: navigation.getParam("categoryTitle", "A Nested Details Screen")
     };
   };
 
   render() {
-    const {categoryId} = this.props.route.params;
+    const { categoryId } = this.props.route.params;
     return (
       <ScrollView horizontal={true} contentContainerStyle={styles.container}>
         <OverviewTopicItem
