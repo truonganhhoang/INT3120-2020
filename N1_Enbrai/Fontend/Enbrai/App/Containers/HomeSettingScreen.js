7bc768b660c1a0651bb42d54b116d9c527a90329
Ngoc Linh <48757937+ngoclinh1999@users.noreply.github.com>
2020-03-23 16:17:05 +0700

    Setting

---
+++ N1_Enbrai/Fontend/Enbrai/App/Containers/HomeSettingScreen.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import HomeSetting from '../Components/HomeSetting'
+export default class HomeSettingScreen extends Component {
+    render() {
+        return (
+            <HomeSetting {...this.props}/>
+        )
+    }
+}
---
+++ N1_Enbrai/Fontend/Enbrai/App/Containers/HomeStudyScreen.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import HomeStudy from '../Components/HomeStudy'
+export default class HomeStudyScreen extends Component {
+    render() {
+        return (
+            <HomeStudy {...this.props}/>
+        )
+    }
+}
