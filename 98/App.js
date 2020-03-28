a61e75428352687badc601037cd99b3373c6ffb0
Maivu1611 <42961556+Maivu1611@users.noreply.github.com>
2020-03-28 16:45:21 +0700

    Add HomeScreen

--- 98/App.js
+++ 98/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 
 class App extends Component {
