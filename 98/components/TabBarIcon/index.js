02be8e65267d5af67438c1d0239d514ee21b581e
DoTuanBac <43380137+DoTuanBac@users.noreply.github.com>
2020-03-30 02:30:56 +0700

    Project mới

--- 98/components/TabBarIcon/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import TabBarIcon from './TabBarIcon';
-
-export const HomeTabBarIcon = () => <TabBarIcon />
-export const ProfileTabBarIcon = () => <TabBarIcon />
-export const SettingTabBarIcon = () => <TabBarIcon />
