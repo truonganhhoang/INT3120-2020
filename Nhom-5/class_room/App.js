0e8dd18084ec0dc27aeee62add7b1584319164c8
anhtuyenuet <nguyenhoangtuyen98@gmail.com>
2020-03-30 02:17:38 +0700

    Update 30-3

--- Nhom-5/class_room/App.js
+++ Nhom-5/class_room/App.js
@@ -1,28 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
+import Card from './components/Card';
+import {createAppContainer} from 'react-navigation';
+import AppNavigator from './AppNavigator';
+
+const AppContainer = createAppContainer(AppNavigator);
 
 class App extends React.Component {
   render() {
-    return (
-      <View>
-        <Text>Some thing here </Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+});
+
 export default App;
