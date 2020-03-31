a83671ebc7e73942b7dfc633dc4e8fb513d2d83a
phamthuy1320 <48676769+phamthuy1320@users.noreply.github.com>
2020-03-31 15:56:08 +0700

    Add files via upload

---
+++ 22/components/DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database name here.
+$DatabaseName = "3000_tu_vung";
+
+//Define your database username here.
+$HostUser = "root";
+
+//Define your database password here.
+$HostPass = "";
+
+?>
