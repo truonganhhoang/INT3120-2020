ac24b582337aa798da66dc95f7c4a12b71b7081d
vuchuc781999 <vuchuc781999@gmail.com>
2020-03-31 21:00:25 +0700

    31/03

--- 88/app/src/components/Navigation.js
+++ 88/app/src/components/Navigation.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text, Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { getStatusBarHeight } from '../utils/StatusBar';
 
 const ReverseIcon = () => {
+  const [ arrow, isArrow ] = useState(false);
+
   return (
     <Animated.View>
       
@@ -14,7 +16,10 @@ const ReverseIcon = () => {
 const Navigation = () => {
   return (
     <View style={styles.navContainer}>
-      <Icon name='navicon' size={20} style={styles.navIcon} />
+      <View>
+        <Icon name='navicon' size={20} style={styles.navIcon} />
+        <Icon name='arrow-right' size={20} style={[styles.navIcon, styles.arrow]} />
+      </View>
       <Text style={styles.navHeader}>JavaScript Tutorial</Text>
     </View>
   );
@@ -25,22 +30,30 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: '#00BCD4',
     alignItems: 'flex-end',
-    paddingTop: getStatusBarHeight()
+    paddingTop: getStatusBarHeight(),
+    paddingLeft: 50
   },
   navIcon: {
-    flex: 1,
+    position: 'absolute',
     color: '#ffffff',
     textAlign: 'center',
     paddingTop: 15,
-    height: 50
-  }, 
+    height: 50,
+    width: 50,
+    marginTop: -50,
+    marginLeft: -50,
+  },
+  arrow: {
+    opacity: 0
+  },
   navHeader: {
-    flex: 6,
+    flex: 1,
     fontSize: 20,
     color: '#ffffff',
     fontWeight: '600',
     height: 50,
-    paddingTop: 11
+    paddingTop: 11,
+    paddingLeft: 1
   }
 })
 
