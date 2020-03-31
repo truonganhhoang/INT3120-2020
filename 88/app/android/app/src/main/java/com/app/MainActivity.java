9286d762cf5841f6b79367d91931e58f07f24359
vuchuc781999 <37411808+vuchuc781999@users.noreply.github.com>
2020-03-31 19:27:47 +0700

    31/03

--- 88/app/android/app/src/main/java/com/app/MainActivity.java
+++ 88/app/android/app/src/main/java/com/app/MainActivity.java
@@ -2,11 +2,6 @@ package com.app;
 
 import com.facebook.react.ReactActivity;
 
-// install react-native-gesture-handler
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
 public class MainActivity extends ReactActivity {
 
   /**
@@ -16,16 +11,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "app";
-  }
-
-  // install react-native-gesture-handler
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
   }
 }
--- 88/app/src/index.js
+++ 88/app/src/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Dimensions } from 'react-native';
-import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
+import React, { useRef } from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, DrawerLayoutAndroid } from 'react-native';
 
 import Navigation from './components/Navigation';
 import Drawer from './components/Drawer'
 
 const Main = () => {
+  const drawer = useRef(null);
+
+  const openDrawer = () => {
+    drawer.current.openDrawer();
+  }
+
   return (
-    <SafeAreaView style={styles.mainContainer}>
-      <DrawerLayout
-        drawerWidth={280}
-        drawerPosition={DrawerLayout.positions.Left}
-        drawerType='front'
-        drawerBackgroundColor='#ddd'
-        renderNavigationView={Drawer}>
-        <Navigation/>
-      </DrawerLayout>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle='light-content' translucent backgroundColor='transparent' />
+      <SafeAreaView style={styles.mainContainer}>
+        <DrawerLayoutAndroid
+          drawerWidth={280}
+          drawerBackgroundColor='#ddd'
+          renderNavigationView={Drawer}
+          ref={drawer}
+        >
+          <Navigation openDrawer={openDrawer}/>
+        </DrawerLayoutAndroid>
+      </SafeAreaView>
+    </> 
   );
 }
 
