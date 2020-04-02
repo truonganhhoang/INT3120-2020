7e9c85e3de35c58503097b10b955da9be3d54781
Maivu1611 <42961556+Maivu1611@users.noreply.github.com>
2020-04-02 18:07:08 +0700

    Update Traffic Sign List Items

--- 98/Client/components/TFS/TFSList.js
+++ 98/Client/components/TFS/TFSList.js
@@ -1,29 +1,45 @@
 import * as React from 'react';
-import {  View, FlatList, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
-
+import { View, FlatList, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 
 
 
 const trafficSignData = [
     {
         id: '1',
-        title: 'Biển đỏ',
+        title: 'Biển cấm',
         image: require('../../assets/images/home/biendo.jpg'),
         route:'Biển đỏ',
     },
     {
         id: '2',
-        title: 'Biển xanh',
+        title: 'Biển báo nguy hiểm',
         image: require('../../assets/images/home/bienxanh.jpg'),
         route:'Biển xanh',
     },
      {
         id: '3',
-        title: 'Biển vàng',
+        title: 'Biển hiệu lệnh',
         image: require('../../assets/images/home/bienvang.jpg'),
         route:'Biển vàng',
     },
-  
+    {
+        id: '4',
+        title: 'Biển chỉ dẫn',
+        image: require('../../assets/images/home/biendo.jpg'),
+        route: 'Biển vàng',
+    },
+    {
+        id: '5',
+        title: 'Biển phụ',
+        image: require('../../assets/images/home/bienxanh.jpg'),
+        route: 'Biển vàng',
+    },
+    {
+        id: '6',
+        title: 'Vạch kẻ đường',
+        image: require('../../assets/images/home/bienvang.jpg'),
+        route: 'Biển vàng',
+    },
 ];
 
   
@@ -31,19 +47,14 @@ function Item( props) {
     const {route,image,title,navigation}=props;
     return (
         
-       <TouchableOpacity style={styles.item}   onPress={() => {
+       <TouchableOpacity onPress={() => {
         navigation.navigate('Detail', { route: route });
       }} >
-                <View >
-                    <View >
-                        <Text style={styles.item_title}>{title}</Text>
-                    </View>
-
-                    <View>
-                     <Image style={styles.tinyLogo} source={image}/>    
-                    </View>
-                </View>
-            </TouchableOpacity>
+            <View style={styles.item}>
+                <Image style={styles.tinyLogo} source={image} />    
+                <Text style={styles.item_title}>{title}</Text>
+            </View>
+       </TouchableOpacity>
     );
 }
 
@@ -58,27 +69,33 @@ export default function TrafficSignList(props) {
                 keyExtractor={(item) => item.id}
                 data={trafficSignData}
                 renderItem={({ item }) => 
-                <Item title={item.title} route={item.route} image={item.image}  navigation={navigation}/>}
+                <Item title={item.title} route={item.route} image={item.image}  navigation={navigation}/> }
             />
         </View>
     );
 }
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: '#dcdcdc',
-        width: null,
-        marginBottom:20,
+        paddingVertical: 3,
+        borderBottomWidth: 1.2,
+        borderColor: '#d3d3d3',
+        flexDirection: 'row',
+        //elevation: 0.8,
     },
  
     item_title: {
         color: '#696969',
-        fontSize: 20,
+        fontSize: 18,
         fontWeight: 'bold',
-        textAlign: 'center',
+        marginLeft: 15,
+        marginTop: 9,
+        //textAlign: 'center',
     },
     tinyLogo: {
-        width: null,
-        height: 200,
-        resizeMode: 'cover',
-      },
+        width: 50,
+        height: 50,
+        //borderColor: "#CED0CE",
+        //borderWidth: 1,
+        marginLeft: 13,
+    },
 });
