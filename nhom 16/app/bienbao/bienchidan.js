d3eabf60dc5cc218bf387c62186e8e8c85d71b4a
tiencttlhp <44044035+tiencttlhp@users.noreply.github.com>
2020-04-04 01:29:01 +0700

    nhóm 16
    app

--- nhom 16/app/bienbao/bienchidan.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Text, View ,StyleSheet,Image, Button} from 'react-native';
-export default class bienchidan extends Component{
-    render(){
-        return(
-            <View>
-                <Text>hello</Text>
-            </View>
-        )}}
