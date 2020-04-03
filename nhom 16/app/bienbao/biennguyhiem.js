e85bc1b72013f53a741d65fe24d16167a7565fa3
tiencttlhp <44044035+tiencttlhp@users.noreply.github.com>
2020-04-03 09:40:33 +0700

    nhóm 16 sửa
    sửa

--- nhom 16/app/bienbao/biennguyhiem.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Text, View ,StyleSheet,Image, Button} from 'react-native';
-export default class biennguyhiem extends Component{
-    render(){
-        return(
-            <View>
-                <Text>hello</Text>
-            </View>
-        )}}
