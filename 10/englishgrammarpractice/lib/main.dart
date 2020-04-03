ba5239c8c358f9d0811eaa0f6868d3f7a0217ea7
suphit <souphitpmc@gmail.com>
2020-04-03 12:26:00 +0700

    change language

--- 10/englishgrammarpractice/lib/main.dart
+++ 10/englishgrammarpractice/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'ທ່ານໄດ້ກົດປຸ່ມບວກຫລາຍຄັ້ງແລ້ວ:',
             ),
             Text(
               '$_counter',
