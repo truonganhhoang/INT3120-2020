0369c87643d0d03380173539b2f0d04f7cc24c34
phamquyetthang <17021033@vnu.edu.vn>
2020-04-03 16:17:51 +0700

    đổi menu, vuốt qua lại các trang

--- nhom100/MyApp/src/components/screens/MenuTab.js
+++ nhom100/MyApp/src/components/screens/MenuTab.js
@@ -1,49 +1,68 @@
 import * as React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Screen1 from './Screen1';
 import Screen2 from './Screen2';
 import Screen3 from './Screen3';
 import Screen5 from './Screen5';
 import Screen4 from './Screen4';
-const Tab = createBottomTabNavigator();
-
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 export default function App() {
   return (
-    <NavigationContainer style={{height:200}}>
+    <NavigationContainer>
       <Tab.Navigator
+        tabBarPosition = "bottom"
         tabBarOptions={{
           activeTintColor: 'hotpink',
           inactiveTintColor: 'gray',
           activeBackgroundColor: 'pink',
           inactiveBackgroundColor: '#eee',
+          showIcon: true,
+          style: {height: 55}
         }}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({color, size }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'D/M') {
-              iconName = 'th';
-            }else if (route.name === 'Cart') {
-              iconName = 'shopping-cart';
-            }else if (route.name === 'T/B') {
-              iconName = 'bell';
-            }else if (route.name === 'Acc') {
-              iconName = 'user';
-            }
-            return <FontAwesome5 name={iconName} size={size} color={color} />;
-          },
-        })}
       >
-        <Tab.Screen name="Home" component={Screen1} />
-        <Tab.Screen name="D/M" component={Screen2} />
-        <Tab.Screen name="Cart" component={Screen3} />
-        <Tab.Screen name="T/B" component={Screen4} />
-        <Tab.Screen name="Acc" component={Screen5} />
+        <Tab.Screen name="Home" 
+          component={Screen1} 
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="home" size={22} color={color} />
+            ),}}
+        />
+        <Tab.Screen name="D/M" component={Screen2}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="th" size={22} color={color} />
+            ),}}
+        />
+        <Tab.Screen name="Cart" component={Screen3} 
+          options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="shopping-cart" size={22} color={color} />
+          ),}}
+        />
+        <Tab.Screen name="T/B" component={Screen4}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="bell" size={22} color={color} />
+          ),}}
+        />
+        <Tab.Screen name="Acc" component={Screen5}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user" size={22} color={color} />
+          ),}}
+        />
       </Tab.Navigator>
-    </NavigationContainer>
+     </NavigationContainer>
+
+
   );
 }
