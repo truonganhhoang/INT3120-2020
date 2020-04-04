be24627e4a136d10bea5c9f8854eea1fec06f3ff
suphit <souphitpmc@gmail.com>
2020-04-04 18:05:21 +0700

    Create sub pages in test directory

--- 10/englishgrammarpractice/lib/pages/about.dart
+++ 10/englishgrammarpractice/lib/pages/about.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:englishgrammarpractice/pages/learn.dart';
+
 
 class About extends StatelessWidget{
   @override
@@ -14,29 +14,25 @@ class About extends StatelessWidget{
               leading:Icon(Icons.info, color: Colors.lightBlue),
               title:Text('Môn phát triển ứng dụng di động',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold), ),
             ),
-            Text('Thành viên nhóm 10 gồm có : ',style: TextStyle(fontSize: 18)),
+            Text('Thành viên nhóm 10 gồm có : ',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold)),
             
             Text('1. 17020680 - Cao Quý Đăng',style: TextStyle(fontSize: 16)),
             Text('2. 17020190 - Sụ phít Phôm ma chăn',style: TextStyle(fontSize: 16)),
             Text('3. 17020658 - Nguyễn Xuân Dương',style: TextStyle(fontSize: 16)),
             Text('\n'),
-            Text('"Bài Tập Ngữ Pháp Tiếng Anh Có Giải Thích"',style: TextStyle(fontSize: 18)),
+            Text('"Bài Tập Ngữ Pháp Tiếng Anh Có Giải Thích"',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
+            
           
-            ListTile(
-              title:Text('Đó là tên ứng dụng của chúng tôi, trong ứng dụng đó người dùng có thể :'),
-            ),
-            ListTile(
-              title:Text('1. - Tra cứu các công thức ngữ pháp tiếng Anh, học lý thuyết ngữ pháp tiếng Anh'),
-            ),
-            ListTile(
-              title:Text('2. - Làm cài bài tập ngữ pháp tiếng anh có hướng dẫn giải'),
-            ),
-            ListTile(
-              title:Text('3. - Kiểm tra, ghi nhớ ngữ pháp lâu hơn thông qua các bài kiểm tra tổng hợp'),
-            ),
+            Text('Đó là tên ứng dụng của chúng tôi, trong ứng dụng đó người dùng có thể :',style: TextStyle(fontSize: 16)),
+            Text('1. Tra cứu các công thức ngữ pháp tiếng Anh',style: TextStyle(fontSize: 16)),
+          
+            Text('2. Làm bài tập ngữ pháp tiếng anh có hướng dẫn giải',style: TextStyle(fontSize: 16)),
+            Text('3. học lý thuyết ngữ pháp tiếng Anh',style: TextStyle(fontSize: 16)),
+            Text('4. Kiểm tra, ghi nhớ ngữ pháp lâu hơn thông qua các bài kiểm tra tổng hợp',style: TextStyle(fontSize: 16)),
+            Text('\n'),
             
             RaisedButton(
-            child: Text('Back'),
+            child: Text('Quay lại'),
             onPressed: () => Navigator.pop(context),
             )
             
---
+++ 10/englishgrammarpractice/lib/pages/test/bai10.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Bai10 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Bài kiểm tra số 10'),
+        ),
+        body: Container(
+          child: RaisedButton(
+            child: Text('Back'),
+            onPressed: () => Navigator.pop(context),
+            )
+        ),
+    );
+  }
+}
