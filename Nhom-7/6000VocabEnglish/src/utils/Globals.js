337168c1de86cb2192f2a8c26aac5a86e6da3628
VuVanHoc <hocvanvu1999@gmail.com>
2020-04-05 13:25:54 +0700

    commit to merge

--- Nhom-7/6000VocabEnglish/src/utils/Globals.js
+++ Nhom-7/6000VocabEnglish/src/utils/Globals.js
@@ -1,8 +1,11 @@
 module.exports = {
-    COLOR: {
-      ORANGE: '#ffa500',
-      DARKBLUE: '#0F3274',
-      LIGHTBLUE: '#6EA8DA',
-      DARKGRAY: '#999',
-    },
-  };
+  COLOR: {
+    ORANGE: "#ffa500",
+    DARKBLUE: "#0F3274",
+    LIGHTBLUE: "#6EA8DA",
+    DARKGRAY: "#999",
+  },
+  QUESTION_TYPE: {
+    SELECTONE: 1,
+  },
+};
