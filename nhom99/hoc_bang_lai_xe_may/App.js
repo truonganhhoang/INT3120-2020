efeaaa5b8aa4c1017991eb50cccceb1248ddef79
Hopm10 <hopm10999@gmail.com>
2020-04-05 21:39:20 +0700

    addScreen

--- nhom99/hoc_bang_lai_xe_may/App.js
+++ nhom99/hoc_bang_lai_xe_may/App.js
@@ -1,31 +1,47 @@
-import * as React from "react";
-import { View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Text } from "./sharedComponents/Text";
-import HomeScreen from "./screens/homeScreen/homeScreen";
-import appColors from "./constants/colors";
+import React from 'react'
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+// import { render } from 'react-dom';
+// import appIcons from '../constants/AppIcons'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import MotobikeScreen from '../trafficlawscreen/motobikeScreen';
+import CarScreen from '../trafficlawscreen/carScreen';
+import MoreScreen from '../trafficlawscreen/moreScreen';
 
-const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: appColors.appGreen
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "normal"
-          }
-        }}
-      >
-        <Stack.Screen name="Ôn thi giấy phép lái xe" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+// function MyTabs() {
+  
+// }
+
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+export default function TrafficLaws() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Xe máy " component={MotobikeScreen} />
+          <Tab.Screen name="Ô tô" component={CarScreen} />
+          <Tab.Screen name="Khác" component={MoreScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      );
+   
 }
-
-export default App;
+// const styles = StyleSheet.create({
+//     container: {
+//         flexDirection: 'row',
+//         flexWrap: 'wrap',
+//         marginTop: 50
+//     },
+//     item: {
+//         width: '33.33%',
+//         height: SCREEN_HEIGHT / 5,
+//         alignItems: "center"
+//       },
+//     text: {
+//         padding: 5,
+//         textAlign: 'center'
+//     }
+// });
