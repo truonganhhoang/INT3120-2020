722d3e59e955ad726fb2dd65593370a6bd68cbfa
DoTuanBac <43380137+DoTuanBac@users.noreply.github.com>
2020-04-06 23:17:00 +0700

    ok commit

--- 98/Sever/app.js
+++ 98/Sever/app.js
@@ -10,89 +10,132 @@ const app = express();
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 app.use(upload());
-app.listen(3000,function()
-{
+app.listen(3000, function () {
   console.log("hello");
 })
-app.set("view engine","ejs");
-app.set("views","./views/pages");
+app.set("view engine", "ejs");
+app.set("views", "./views/pages");
 //End use
 //End setting
 
 
 //Models
-var TFS=require('./models/TrafficSign');
-var TFSC=require('./models/trafficSignCategory');
-var mySQL=require('./models/MySQL');
+var TFS = require('./models/TrafficSign');
+var TFSC = require('./models/trafficSignCategory');
+var mySQL = require('./models/MySQL');
 
 //End models
 //Use model
-var con= mySQL.conn();
+var con = mySQL.conn();
 //End use
 //Route
-app.get("/",function(req,res)
-{
+app.get("/", function (req, res) {
 
   res.send("ok");
- 
+
 })
 
-app.get("/admin",function(req,res)
-{
+app.get("/admin", function (req, res) {
   res.render("admin/admin");
 })
 
 //Admin traffic signs
-app.get("/admin/trafficsigns",function(req,res)
-{
-  
-  res.render("admin/TrafficSigns/listTrafficSigns");
+app.get("/admin/trafficsigns", function (req, res) {
+
+  TFS.list(con, "", function (result) {
+    res.render("admin/TrafficSigns/list", { result });
+  });
+
+
 })
 
-app.get("/admin/addtrafficsigns",function(req,res)
-{
-TFS.listCategoriesId(con,function(result)
-{
- res.render("admin/TrafficSigns/addtrafficsigns",{result});
-})
- 
+app.get("/admin/trafficsigns/add", function (req, res) {
+  TFS.listCategoriesId(con, function (result) {
+    res.render("admin/TrafficSigns/add", { result });
+  })
+
 });
 
-app.get("/admin/trafficsigns/json/:name",function(req,res)
-{
-  var category=req.params.name;
-  TFS.list(con,res,category);
+app.get("/admin/trafficsigns/json/:name", function (req, res) {
+  var id = req.params.name;
+  TFS.list(con, id, function (result) {
+    res.send(result);
+  });
+});
+app.get("/admin/trafficsigns/change/:name", function (req, res) {
+  var id = req.params.name;
+  TFS.listCategoriesId(con, function (result) {
+    TFS.list(con, id, function (result2) {
+      res.render("admin/TrafficSigns/change", { result, result2 });
+    });
+
+  })
 });
 
+app.post('/admin/trafficsigns/change/:name', (req, res) => {
+  TFS.change(req, con);
+  res.redirect('/admin/trafficsigns');
 
+});
+app.get('/admin/trafficsigns/delete/:name', (req, res) => {
+  var id = req.params.name;
+  TFS.delete(id, con);
+  res.redirect('/admin/trafficsigns');
 
+});
+app.post('/admin/trafficsigns/add', (req, res) => {
 
-app.post('/admin/addtrafficsigns', (req, res) => {
- 
-  TFS.add(req,res,con);
+  TFS.add(req, con);
+  res.redirect('/admin/trafficsigns');
 
 });
 //////////////
 
 //Admin traffic signs categories
-app.get("/admin/trafficsignCategories",function(req,res)
-{
-  res.render("admin/trafficsignCategories/listtrafficsignCategories");
+app.get("/admin/trafficsignCategories", function (req, res) {
+  TFSC.list(con,"", function (result) {
+    res.render("admin/trafficsignCategories/list", { result });
+  });
+
+
 })
-app.get("/admin/addtrafficsignCategories",function(req,res)
-{
-  res.render("admin/trafficsignCategories/addtrafficsignCategories");
+app.get("/admin/trafficsignCategories/add", function (req, res) {
+  res.render("admin/TrafficSignCategories/add");
 })
 
-app.get("/admin/trafficsignCategories/json/",function(req,res)
-{
+app.get("/admin/trafficsignCategories/json/", function (req, res) {
 
-//res.send('User name có tên: '+ req.params.name);
-  TFSC.list(con,res);
+  TFSC.list(con,"", function (result) {
+    res.send(result);
+
+  });
+
 })
-app.post('/admin/addtrafficsignCategories', (req, res) => {
- 
-  TFSC.add(req,res,con);
+app.post('/admin/trafficsignCategories/add', (req, res) => {
+
+  TFSC.add(req, con);
+  res.redirect('/admin/trafficsignCategories');
+});
+
+app.get("/admin/trafficsignCategories/change/:name", function (req, res) {
+  var id = req.params.name;
+
+    TFSC.list(con, id, function (result) {
+
+      res.render("admin/trafficsignCategories/change", { result });
+    });
+
+
+});
+app.post('/admin/trafficsignCategories/change/:name', (req, res) => {
+  TFSC.change(req, con);
+  res.redirect('/admin/trafficsignCategories');
+
+});
+app.get('/admin/trafficsignCategories/delete/:name', (req, res) => {
+  var id = req.params.name;
+  TFSC.delete(id, con);
+  res.redirect('/admin/trafficsignCategories');
 
 });
 //////////////
--- 98/Sever/models/TrafficSign.js
+++ 98/Sever/models/TrafficSign.js
@@ -1,6 +1,6 @@
 const mkdirp = require('mkdirp');
 
-exports.add=function(req,res,con)
+exports.add=function(req,con)
 {
    
     if(req.files)
@@ -16,7 +16,7 @@ exports.add=function(req,res,con)
         var sql = "INSERT INTO trafficsigns (tfs_category_id, tfs_name,tfs_image,tfs_content) VALUES (?,?,?,?)";
         con.query(sql,[category,name,"/public/images/trafficsigns/"+category+"/"+filename,content], function (err, result) {
          if (err) throw err;
-         res.send("Thành công");
+    
 
   });
   
@@ -24,15 +24,17 @@ exports.add=function(req,res,con)
 }
 
 
-exports.list=function(con,res,category)
+exports.list=function(con,id,cb)
 {
-    var temp= con.query("SELECT * FROM trafficsigns WHERE tfs_category_id =?",category, function (err, result, fields) {
+  if(id!="")
+  var query="SELECT * FROM trafficsigns WHERE tfs_id =?";
+  else
+  {
+    var query="SELECT * FROM trafficsigns";
+  }
+    var temp= con.query(query,id, function (err, result, fields) {
         if (err) throw err;
-        var test = 
-        {
-            data:result
-        }
-        res.send( test );
+        cb(result)
       });
     
 
@@ -47,4 +49,46 @@ exports.listCategoriesId=function(con,cb)
 
       });
      
+}
+
+
+exports.change=function(req,con)
+{
+  var id = req.body.TFS_id;
+  var name = req.body.TFS_name;
+  var category = req.body.TFS_category;
+ var content =  req.body.TFS_content;
+console.log(id,name,category,content)
+    if(req.files)
+    {
+       var file =req.files.TFS_file;
+       var filename= file.name;
+        file.mv("./public/images/trafficsigns/"+category+"/"+filename);
+        var sql = "UPDATE trafficsigns SET tfs_category_id=?, tfs_name=?,tfs_image=?,tfs_content=? WHERE tfs_id=?";
+        con.query(sql,[category,name,"/public/images/trafficsigns/"+category+"/"+filename,content,id], function (err, result) {
+         if (err) throw err;
+       
+
+  });
+  
+    }
+    else
+    {
+      var sql = "UPDATE trafficsigns SET tfs_category_id=?, tfs_name=?,tfs_content=? WHERE tfs_id=?";
+      con.query(sql,[category,name,content,id], function (err, result) {
+       if (err) throw err;
+
+      });
+    }
+}
+
+exports.delete=function(id,con)
+{
+    
+      var sql = "DELETE FROM trafficsigns  WHERE tfs_id=?";
+      con.query(sql,[id], function (err, result) {
+       if (err) throw err;
+     
+      });
+    
 }
