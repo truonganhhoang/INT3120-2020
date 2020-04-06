77bebfada8e830492313e91166d10d3721a41309
NgocNam3518 <ngocnam3518@gmail.com>
2020-04-07 01:00:03 +0700

    failed

--- Group3/TOEIC_TEST/App/Components/Notification.js
+++ Group3/TOEIC_TEST/App/Components/Notification.js
@@ -10,7 +10,7 @@ class Notification extends Component {
         super(props)
     }
 
-        componentDidMount() {
+    componentDidMount() {
         fcmService.register(this.onRegister, this.onNotification, 
         this.onOpenNotification)
     }
