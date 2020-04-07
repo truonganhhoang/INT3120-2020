d33f113e33fc535c0bd6aa17ac8c167d06ef4923
GonDaR <zzphonixzz@gmail.com>
2020-04-07 16:33:01 +0700

    remove file

--- N1_Enbrai/Fontend/Enbrai/App/Containers/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import Home from '../Components/Home'
-export default class HomeScreen extends Component {
-    render() {
-        return (
-            <Home {...this.props}/>
-        )
-    }
-}
--- N1_Enbrai/Fontend/Enbrai/App/Containers/HomeUpdateScreen.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import HomeUpdate from '../Components/HomeUpdate'
-export default class HomeUpdateScreen extends Component {
-    render() {
-        return (
-            <HomeUpdate {...this.props}/>
-        )
-    }
-}
