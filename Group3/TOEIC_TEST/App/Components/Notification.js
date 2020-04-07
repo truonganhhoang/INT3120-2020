367d7c287f39f37c89ffb338fde6c22a54f8177f
Lê Văn Đức <leducuet@gmail.com>
2020-04-08 00:16:27 +0700

    undo commit

--- Group3/TOEIC_TEST/App/Components/Notification.js
+++ Group3/TOEIC_TEST/App/Components/Notification.js
@@ -3,16 +3,16 @@ import {
     View,
     Text,
 } from 'react-native'
-import {fcmService} from '../Services/FCMService';
+import { fcmService } from '../Services/FCMService';
 
 class Notification extends Component {
     constructor(props) {
         super(props)
     }
 
-        componentDidMount() {
-        fcmService.register(this.onRegister, this.onNotification, 
-        this.onOpenNotification)
+    componentDidMount() {
+        fcmService.register(this.onRegister, this.onNotification,
+            this.onOpenNotification)
     }
 
     onRegister(token) {
@@ -23,23 +23,23 @@ class Notification extends Component {
         console.log("[NotificationFCM] onNotification: ", notify)
         // For Android
         const channelObj = {
-        channelId: "SampleChannelID",
-        channelName: "SampleChannelName",
-        channelDes: "SampleChannelDes"
+            channelId: "SampleChannelID",
+            channelName: "SampleChannelName",
+            channelDes: "SampleChannelDes"
         }
         const channel = fcmService.buildChannel(channelObj)
 
         const buildNotify = {
-        dataId: notify._notificationId,
-        title: notify._title,
-        content: notify._body,
-        sound: 'default',
-        channel: channel,
-        data: {},
-        colorBgIcon: "#1A243B",
-        largeIcon: 'ic_launcher',
-        smallIcon: 'ic_launcher',
-        vibrate: true
+            dataId: notify._notificationId,
+            title: notify._title,
+            content: notify._body,
+            sound: 'default',
+            channel: channel,
+            data: {},
+            colorBgIcon: "#1A243B",
+            largeIcon: 'ic_launcher',
+            smallIcon: 'ic_launcher',
+            vibrate: true
         }
 
         const notification = fcmService.buildNotification(buildNotify)
