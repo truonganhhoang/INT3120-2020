cdb9f8d8deaff885518cd870e1626c3a2b94d5e2
duong755 <ngo.quang.duong.1100@gmail.com>
2020-04-12 00:30:25 +0700

    Put enableProdMode() in try ... catch ...

--- 12/src/main.ts
+++ 12/src/main.ts
@@ -1,6 +1,16 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  try {
+    enableProdMode();
+  } catch {
+    // ignore error
+  }
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
