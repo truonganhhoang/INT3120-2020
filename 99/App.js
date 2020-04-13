59f8694d520f240eef8ff0baca90d14fd91fb017
Hopm10 <hopm10999@gmail.com>
2020-04-13 21:34:25 +0700

    add tiScreen

--- 99/App.js
+++ 99/App.js
@@ -1,13 +1,21 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Text } from "./sharedComponents/Text";
 import HomeScreen from "./screens/homeScreen/homeScreen";
 import appColors from "./constants/colors";
 import TrafficLaws from "./screens/trafficlawscreen/TrafficLaws";
+// import Accordian from "./component/Accordian";
+// import ListAccordians from "../99/component/ListAccordians";
+// import Colors from "../99/component/Colors";
+// import {Component} from 'react';
+import tipScreen from "../99/screens/tipScreen/TipScreen"
 
 const Stack = createStackNavigator();
+
+
+
 
 function App() {
   return (
@@ -36,9 +44,22 @@ function App() {
           component={TrafficLaws}
           options={{ title: "Luật giao thông", headerTitleAlign: "center" }}
         />
+        <Stack.Screen
+          name="tipScreen"
+          component={tipScreen}
+          options={{ title: "Mẹo thi kết quả cao", headerTitleAlign: "center" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
+
 }
 
 export default App;
+
+// function App (){
+//   return (
+//     <ListAccordians/>
+//   );
+// };
+// export default App
---
+++ 99/screens/tipScreen/Accordian.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import { View, TouchableOpacity, Text, StyleSheet} from "react-native";
+import { Colors } from "./Colors";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+export default class Accordian extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+          data: props.data,
+          expanded : false,
+        }
+    }
+  
+  render() {
+
+    return (
+       <View>
+            <TouchableOpacity activeOpacity= {0.8} style={styles.row} onPress={()=>this.toggleExpand()}>
+                <Text style={[styles.title, styles.font]}>{this.props.title}</Text>
+                <Icon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} color={Colors.DARKGRAY} />
+            </TouchableOpacity>
+            <View style={styles.parentHr}/>
+            {
+                this.state.expanded &&
+                <View style={styles.child}>
+                    <Text style= {styles.text}>{this.props.data}</Text>    
+                </View>
+            }
+            
+       </View>
+    )
+  }
+
+  toggleExpand=()=>{
+    this.setState({expanded : !this.state.expanded})
+  }
+
+}
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 15,
+        fontWeight:'bold',
+        color: Colors.DARKGRAY,
+    },
+    row:{
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        height:56,
+        paddingLeft:25,
+        paddingRight:18,
+        marginVertical: 3,
+        marginHorizontal: 10,
+        borderRadius: 10,
+        alignItems:'center',
+        backgroundColor: Colors.CGRAY,
+    },
+    parentHr:{
+        height:0,
+        color: Colors.WHITE,
+        width:'100%'
+    },
+    child:{
+        backgroundColor: '#1abc9c',
+        padding:16,
+    },
+    text: {
+        fontSize: 14,
+        color: 'white',
+    }
+    
+});
