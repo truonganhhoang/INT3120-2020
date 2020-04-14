b45779154a38680bee110ba6de15c1eb5ab5a95a
duong755 <ngo.quang.duong.1100@gmail.com>
2020-04-14 18:10:15 +0700

    UI for course detail page

--- 12/src/app/tabs/learn/course-detail/course-detail.module.ts
+++ 12/src/app/tabs/learn/course-detail/course-detail.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,7 +10,7 @@ import { CourseDetailPageRoutingModule } from './course-detail-routing.module';
 import { CourseDetailPage } from './course-detail.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, CourseDetailPageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, MatTabsModule, CourseDetailPageRoutingModule],
   declarations: [CourseDetailPage]
 })
 export class CourseDetailPageModule {}
--- 12/src/app/tabs/learn/course-detail/overview/overview.page.ts
+++ 12/src/app/tabs/learn/course-detail/overview/overview.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.page.scss']
 })
 export class OverviewPage implements OnInit {
-  constructor() {}
+  courseUrl = '';
+
+  constructor(private router: Router) {
+    this.courseUrl = this.router.url.split('/').reverse().slice(1).reverse().join('/');
+  }
 
   ngOnInit() {}
 }
