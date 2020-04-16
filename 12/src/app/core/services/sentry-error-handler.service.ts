98f98d72b77762668a775a76c225767c88d965d8
duong755 <ngo.quang.duong.1100@gmail.com>
2020-04-16 16:14:04 +0700

    Remove isDevMode() and try ... catch ... in main.ts

--- 12/src/app/core/services/sentry-error-handler.service.ts
+++ 12/src/app/core/services/sentry-error-handler.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, ErrorHandler, isDevMode } from '@angular/core';
+import { Injectable, ErrorHandler } from '@angular/core';
 import * as Sentry from '@sentry/browser';
+import { environment } from '../../../environments/environment';
 
 Sentry.init({
   dsn: 'https://2227874519a54f3eb5bc60a0604cd1bb@o367163.ingest.sentry.io/5196464',
-  environment: isDevMode() ? 'development' : 'production'
+  environment: environment.production ? 'production' : 'development'
 });
 
 @Injectable()
