3930c24be9330bd8658d20ab96bb7913f7f5d6da
HungManh <manh2909@gmail.com>
2020-04-20 21:02:38 +0700

    Nhóm 18 sửa lại code

---
+++ 18/my-project/screens/Policy.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class Policy extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        )
+    }
+}
