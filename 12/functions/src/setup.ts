841b29c45babb1756c6d58eec93fd1e0dce61c1d
duong755 <ngo.quang.duong.1100@gmail.com>
2020-04-24 21:15:34 +0700

    Add sentry node

--- 12/functions/src/setup.ts
+++ 12/functions/src/setup.ts
@@ -1,6 +1,11 @@
+import * as Sentry from '@sentry/node';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import _ from 'lodash';
+
+Sentry.init({
+  dsn: 'https://2227874519a54f3eb5bc60a0604cd1bb@o367163.ingest.sentry.io/5196464'
+});
 
 const app = admin.initializeApp();
 const fn = functions.region('asia-east2');
@@ -11,4 +16,4 @@ const filterUserKeys = (data: any) => {
   return _.pickBy(data, (value, key) => !(key in IGNORED_KEYS));
 };
 
-export { app, admin, fn, filterUserKeys };
+export { app, admin, fn, filterUserKeys, Sentry };
--- 12/functions/src/user.ts
+++ 12/functions/src/user.ts
@@ -1,10 +1,4 @@
-import { pickBy } from 'lodash';
-import { app, admin, fn } from './setup';
-
-const IGNORED_KEYS = ['passwordHash', 'passwordSalt', 'providerData', 'customClaims', 'metadata', 'uid'];
-const filterUserKeys = (data: any) => {
-  return pickBy(data, (value, key) => !(key in IGNORED_KEYS));
-};
+import { app, admin, fn, filterUserKeys, Sentry } from './setup';
 
 const auth = fn.auth;
 
@@ -13,17 +7,29 @@ const isFirebaseUser = (obj: any): obj is admin.auth.UserRecord => {
 };
 
 export const updateUserData = fn.https.onCall((data, context) => {
-  if (isFirebaseUser(data) && context.auth?.uid === data.uid) {
-    const userData = filterUserKeys(data.toJSON());
-    app.firestore().collection('users').doc(data.uid).set(userData);
+  try {
+    if (isFirebaseUser(data) && context.auth?.uid === data.uid) {
+      const userData = filterUserKeys(data.toJSON());
+      app.firestore().collection('users').doc(data.uid).set(userData);
+    }
+  } catch (err) {
+    Sentry.captureException(err.originalError || err);
   }
 });
 
 export const handleCreateUser = auth.user().onCreate((user) => {
-  const userData = filterUserKeys(user.toJSON());
-  app.firestore().collection('users').doc(user.uid).set(userData);
+  try {
+    const userData = filterUserKeys(user.toJSON());
+    app.firestore().collection('users').doc(user.uid).set(userData);
+  } catch (err) {
+    Sentry.captureException(err.originalError || err);
+  }
 });
 
 export const handleDeleteUser = auth.user().onDelete((user) => {
-  app.firestore().collection('users').doc(user.uid).delete();
+  try {
+    app.firestore().collection('users').doc(user.uid).delete();
+  } catch (err) {
+    Sentry.captureException(err.originalError || err);
+  }
 });
