3d7cfc9c119137341f28d232513964d7744f2dbe
Quang Nguyễn <adquang199x@gmail.com>
2020-04-28 06:49:08 +0700

    update static lessions

--- 8/Eng/src/android/screens/WordGroupScreen/WordGroup.tsx
+++ 8/Eng/src/android/screens/WordGroupScreen/WordGroup.tsx
@@ -36,6 +36,7 @@ const WordGroupScreen = (props: { route?: any; navigation?: any }) => {
     )
   }
   else {
+    console.log(nameTopic);
     var index = 0;
     const data: any = [];
     Object.keys(lessons).forEach((item, index) => {
