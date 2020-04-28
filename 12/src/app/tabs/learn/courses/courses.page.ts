1f3c0140be0214f3dd028b3b306d61165bb257e0
duong755 <ngo.quang.duong.1100@gmail.com>
2020-04-29 02:26:49 +0700

    Don't update user data at courses page

--- 12/src/app/tabs/learn/courses/courses.page.ts
+++ 12/src/app/tabs/learn/courses/courses.page.ts
@@ -1,29 +1,13 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { Router, NavigationEnd, RouterEvent } from '@angular/router';
-import { filter } from 'rxjs/operators';
-
-import { UserService } from '../../../core/services/firebase/auth/user.service';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.page.html',
   styleUrls: ['./courses.page.scss']
 })
-export class CoursesPage implements AfterViewInit {
-  navigationEnd$: Observable<RouterEvent>;
-  updateUserDate$: Observable<any>;
-  constructor(private router: Router, private userService: UserService) {
-    this.navigationEnd$ = this.router.events.pipe(filter((event: RouterEvent) => event instanceof NavigationEnd));
-  }
-
-  async ngAfterViewInit() {
-    this.navigationEnd$.subscribe(() => {
-      if (this.router.getCurrentNavigation().extras.state?.fromSignUp) {
-        this.userService.updateUserData().subscribe();
-      }
-    });
-  }
+export class CoursesPage {
+  constructor(private router: Router) {}
 
   goToCourse(courseId: number) {
     this.router.navigate([`/tabs/learn/course/${courseId}`]);
