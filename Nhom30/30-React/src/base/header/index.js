9e94677c3aa32c6194ee7443e7da04d4a48bd4c9
HoanGnR <47739011+HoanGnR@users.noreply.github.com>
2020-04-28 13:21:26 +0700

    Update index.js
    Fixed syntax

--- Nhom30/30-React/src/base/header/index.js
+++ Nhom30/30-React/src/base/header/index.js
@@ -1,207 +1,169 @@
-// import React, { Component } from 'react'
-// import { StyleSheet, Dimensions, Text, Image, View, Alert } from 'react-native'
-// import { TouchableOpacity } from 'react-native-gesture-handler'
-//
-// export default class Header extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//
-//         }
-//     }
-//
-//     handleTitle = (title) => {
-//         switch (title) {
-//             case 'LearnTheoryScreen':
-//                 return 'Học lý thuyết'
-//                 break;
-//             case 'Home':
-//                 return 'Thi thử bằng lái xe a1 2019'
-//                 break;
-//             case 'ListBoard':
-//                 return 'Biển báo đường bộ'
-//                 break;
-//             case 'Tips':
-//                 return 'Mẹo thi đạt kết quả cao'
-//                 break;
-// 	    case 'SearchScreen':
-// 		return 'Tra cứu luật nhanh'
-// 		break;
-//         }
-//     }
-//     handleChooseLeft = () => {
-//         this.props.parent.props.navigation.openDrawer()
-//     }
-//
-//     renderLeft = () => {
-//         if (this.props.parent.props.navigation.state.routeName != 'Home') {
-//             return (
-//                 <TouchableOpacity style={styles.renderLeft.touchableOpacity} onPress={() => this.props.parent.props.navigation.goBack()}>
-//                     <Image
-//                         source={require('../../../icon/icon_back.png')}
-//                         style={styles.renderLeft.image_back}
-//                     />
-//                 </TouchableOpacity>
-//             )
-//         }
-//         return (
-//             <TouchableOpacity style={styles.renderLeft.touchableOpacity} onPress={() => this.handleChooseLeft()}>
-//                 <Image
-//                     source={require('../../../icon/icon_menu.png')}
-//                     style={styles.renderLeft.image}
-//                 />
-//             </TouchableOpacity>
-//         )
-//     }
-//
-//     renderCenter = () => {
-//         if (this.props.parent.props.navigation.state.params) {
-//             return (
-//                 <View style={styles.renderCenter.view}>
-//                     <Text style={styles.renderCenter.text}>{this.props.parent.props.navigation.state.params.label}</Text>
-//                 </View>
-//             )
-//         }
-//         return (
-//             <View style={styles.renderCenter.view}>
-//                 <Text style={styles.renderCenter.text}>{this.handleTitle(this.props.parent.props.navigation.state.routeName)}</Text>
-//             </View>
-//         )
-//     }
-//     handleVIP = () => {
-//         Alert.alert(
-//             'Thi thử GPLX',
-//             'Nâng cấp VIP',
-//             [
-//               {text: 'Huỷ bỏ', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-//               {text: 'Đồng ý', onPress: () => console.log('OK Pressed'), style:'destructive'},
-//             ],
-//             { cancelable: false }
-//           )
-//     }
-//     renderRight = () => {
-//         return (
-//             <TouchableOpacity style={styles.renderRight.touchableOpacity} onPress={this.handleVIP}>
-//                 <Image
-//                     source={require('../../../icon/icon_star.png')}
-//                     style={styles.renderRight.image}
-//                 />
-//             </TouchableOpacity>
-//         )
-//     }
-//     render() {
-//         return (
-//             <View style={styles.block}>
-//                 {this.renderLeft()}
-//                 {this.renderCenter()}
-//                 {this.renderRight()}
-//             </View>
-//         )
-//     }
-// }
-// const { width } = Dimensions.get('window')
-// const styles = {
-//     block: {
-//         width: '100%',
-//         height: 60,
-//         alignItems: 'center',
-//         backgroundColor: 'green',
-//         flexDirection: 'row',
-//         position:'absolute',
-//         top:0,
-//         left:0,
-//         bottom:0,
-//         right:0
-//
-//     },
-//     renderLeft: {
-//         view: {
-//             width: '20%',
-//             height: '100%',
-//         },
-//         touchableOpacity: {
-//             width: width / 5,
-//             height: 60,
-//             justifyContent: 'center',
-//             paddingLeft: 15
-//         },
-//         image: {
-//             width: 25,
-//             height: 25
-//         },
-//         image_back: {
-//             width: 40,
-//             height: 40
-//         }
-//     },
-//     renderCenter: {
-//         view: {
-//             width: '60%',
-//             height: '100%',
-//             alignItems: 'center',
-//             justifyContent: 'center'
-//         },
-//         text: {
-//             fontSize: 17,
-//             color: 'white',
-//             fontWeight: 'bold',
-//             textAlign:'center'
-//         }
-//     },
-//     renderRight: {
-//         view: {
-//             width: '20%',
-//             height: '100%',
-//         },
-//         touchableOpacity: {
-//             width: width / 5,
-//             height: 60,
-//             justifyContent: 'center',
-//             alignItems: 'flex-end',
-//             paddingRight: 10
-//
-//         },
-//         image: {
-//             width: 25,
-//             height: 25
-//         }
-//     },
-// }
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TabViewAnimated, TabViewPage, TabBarTop } from 'react-native-tab-view';
+import React, { Component } from 'react'
+import { StyleSheet, Dimensions, Text, Image, View, Alert } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
-export default class TabViewExample extends Component {
-  state = {
-    index: 0,
-    routes: [
-      { key: '1', title: 'First' },
-      { key: '2', title: 'Second' },
-    ],
-  };
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
 
-  _renderScene = ({ route }) => {
-    switch (route.key) {
-    case '1':
-      return <View style={{ flex: 1, backgroundColor: '#ff4081' }} />;
-    case '2':
-      return <View style={{ flex: 1, backgroundColor: '#673ab7' }} />;
-    default:
-      return null;
+        }
     }
-  };
 
-  _renderPage = (props) => <TabViewPage {...props} renderScene={this._renderScene} />;
+    handleTitle = (title) => {
+        switch (title) {
+            case 'LearnTheoryScreen':
+                return 'Học lý thuyết'
+                break;
+            case 'Home':
+                return 'Thi thử bằng lái xe a1 2019'
+                break;
+            case 'ListBoard':
+                return 'Biển báo đường bộ'
+                break;
+            case 'Tips':
+                return 'Mẹo thi đạt kết quả cao'
+                break;
+	    case 'SearchScreen':
+		return 'Tra cứu luật nhanh'
+		break;
+        }
+    }
+    handleChooseLeft = () => {
+        this.props.parent.props.navigation.openDrawer()
+    }
 
-  render() {
-    return (
-      <TabViewAnimated
-        style={{ flex: 1 }}
-        navigationState={this.state.navigation}
-        renderScene={this._renderPage}
-        renderHeader={this._renderHeader}
-        onRequestChangeTab={index => this.setState({ index })}
-      />
-    );
-  }
+    renderLeft = () => {
+        if (this.props.parent.props.navigation.state.routeName != 'Home') {
+            return (
+                <TouchableOpacity style={styles.renderLeft.touchableOpacity} onPress={() => this.props.parent.props.navigation.goBack()}>
+                    <Image
+                        source={require('../../../icon/icon_back.png')}
+                        style={styles.renderLeft.image_back}
+                    />
+                </TouchableOpacity>
+            )
+        }
+        return (
+            <TouchableOpacity style={styles.renderLeft.touchableOpacity} onPress={() => this.handleChooseLeft()}>
+                <Image
+                    source={require('../../../icon/icon_menu.png')}
+                    style={styles.renderLeft.image}
+                />
+            </TouchableOpacity>
+        )
+    }
+
+    renderCenter = () => {
+        if (this.props.parent.props.navigation.state.params) {
+            return (
+                <View style={styles.renderCenter.view}>
+                    <Text style={styles.renderCenter.text}>{this.props.parent.props.navigation.state.params.label}</Text>
+                </View>
+            )
+        }
+        return (
+            <View style={styles.renderCenter.view}>
+                <Text style={styles.renderCenter.text}>{this.handleTitle(this.props.parent.props.navigation.state.routeName)}</Text>
+            </View>
+        )
+    }
+    handleVIP = () => {
+        Alert.alert(
+            'Thi thử GPLX',
+            'Nâng cấp VIP',
+            [
+              {text: 'Huỷ bỏ', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+              {text: 'Đồng ý', onPress: () => console.log('OK Pressed'), style:'destructive'},
+            ],
+            { cancelable: false }
+          )
+    }
+    renderRight = () => {
+        return (
+            <TouchableOpacity style={styles.renderRight.touchableOpacity} onPress={this.handleVIP}>
+                <Image
+                    source={require('../../../icon/icon_star.png')}
+                    style={styles.renderRight.image}
+                />
+            </TouchableOpacity>
+        )
+    }
+    render() {
+        return (
+            <View style={styles.block}>
+                {this.renderLeft()}
+                {this.renderCenter()}
+                {this.renderRight()}
+            </View>
+        )
+    }
+}
+const { width } = Dimensions.get('window')
+const styles = {
+    block: {
+        width: '100%',
+        height: 60,
+        alignItems: 'center',
+        backgroundColor: 'green',
+        flexDirection: 'row',
+        position:'absolute',
+        top:0,
+        left:0,
+        bottom:0,
+        right:0
+
+    },
+    renderLeft: {
+        view: {
+            width: '20%',
+            height: '100%',
+        },
+        touchableOpacity: {
+            width: width / 5,
+            height: 60,
+            justifyContent: 'center',
+            paddingLeft: 15
+        },
+        image: {
+            width: 25,
+            height: 25
+        },
+        image_back: {
+            width: 40,
+            height: 40
+        }
+    },
+    renderCenter: {
+        view: {
+            width: '60%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        text: {
+            fontSize: 17,
+            color: 'white',
+            fontWeight: 'bold',
+            textAlign:'center'
+        }
+    },
+    renderRight: {
+        view: {
+            width: '20%',
+            height: '100%',
+        },
+        touchableOpacity: {
+            width: width / 5,
+            height: 60,
+            justifyContent: 'center',
+            alignItems: 'flex-end',
+            paddingRight: 10
+
+        },
+        image: {
+            width: 25,
+            height: 25
+        }
+    },
 }
