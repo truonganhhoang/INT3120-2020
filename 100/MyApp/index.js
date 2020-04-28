c80d697ace0d1679a530d49315d681b67473c7ca
phamquyetthang <17021033@vnu.edu.vn>
2020-04-28 11:15:25 +0700

    end test redux

--- 100/MyApp/index.js
+++ 100/MyApp/index.js
@@ -3,20 +3,22 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
+import App from './App';
 import {name as appName} from './app.json';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import allReducers from './src/reducers';
-import TaskManagerComponent from './src/components/components/TaskManagerComponent';
 
 
-let store = createStore(allReducers);
-const App = () =>(
-    <Provider store={store}>
-        <TaskManagerComponent />
-    </Provider>
-);
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import allReducers from './src/reducers';
+// import TaskManagerComponent from './src/components/components/TaskManagerComponent';
+
+
+// let store = createStore(allReducers);
+// const App = () =>(
+//     <Provider store={store}>
+//         <TaskManagerComponent />
+//     </Provider>
+// );
 
 
 
