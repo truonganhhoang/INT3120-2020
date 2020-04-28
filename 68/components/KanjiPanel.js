015be0aff6e82a4d23a8cdfe6eef88a8583392e2
vantu <nguyenvantuhym@gmail.com>
2020-04-28 21:07:04 +0700

    config eslint

--- 68/components/KanjiPanel.js
+++ 68/components/KanjiPanel.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { TouchableOpacity, Dimensions, View, Text, StyleSheet} from 'react-native';
 import firebase from '../config/firebase';
 import Svg, { Circle, Rect, SvgUri } from 'react-native-svg';
@@ -6,19 +6,30 @@ const deviceWidth = Dimensions.get('window').width;
 
 const screen = (percent)=> deviceWidth * percent / 100;
 var storage = firebase.storage();
-var storageRef = storage.ref('imgKanji/19968.svg');
+var storageRef = storage.ref('imgKanji/35506.svg');
 
 export default function Speaker({kanji}){
+    const [uriSvg, setUriSvg] = useState('');
     useEffect(()=>{
         console.log("as");
-        
+        storageRef.getDownloadURL().then((url)=> {
+            // `url` is the download URL for 'images/stars.jpg'
+            setUriSvg(url);
+            console.log(url);
+            // Or inserted into an <img> element:
+
+           
+          }).catch(function(error) {
+            // Handle any errors
+            console.log(error);
+          });
     },[]);
     return (
         <TouchableOpacity
          onPress={()=>{}}>
             <View style={styles.KanjiPanel}>
                 <View style={styles.centerAlighElement}>
-                    <Text style={{color: '#006265', fontSize:100}}>{kanji}</Text>
+                    <SvgUri width="100%" height="100%" uri={`https://firebasestorage.googleapis.com/v0/b/perfect-tape-242508.appspot.com/o/imgKanji/35506.svg?alt=media&token=b93b88d8-43a7-47e2-ad74-6b9a34359067`}/>
                 </View>
             </View>
         </TouchableOpacity>
