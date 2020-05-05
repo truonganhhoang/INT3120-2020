eaf646588507a4a7e2a45d341e683c73352e7838
tiencttlhp <44044035+tiencttlhp@users.noreply.github.com>
2020-05-05 16:17:36 +0700

    nhóm 16
    update

---
+++ nhom 16/app/component/LoiThucHanh.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View ,StyleSheet,Image,ScrollView} from 'react-native';
+import thucHanh from '../assets/thiThucHanh.jpg'
+export default class TrangChu extends Component{
+    render(){
+        return(
+            <ScrollView style={{backgroundColor:'#F5ECCE'}}>  
+                
+            </ScrollView>
+
+        );
+    }
+    
+   }
