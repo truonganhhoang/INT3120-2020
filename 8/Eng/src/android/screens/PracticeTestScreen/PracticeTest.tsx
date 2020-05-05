89aa446e38833b48d396c0a74a4b437815d0fe72
Quang Nguyễn <adquang199x@gmail.com>
2020-05-05 11:50:49 +0700

    add progressbar

--- 8/Eng/src/android/screens/PracticeTestScreen/PracticeTest.tsx
+++ 8/Eng/src/android/screens/PracticeTestScreen/PracticeTest.tsx
@@ -1,40 +1,41 @@
-import React, { useState, useEffect } from 'react'; 
-import { View, Text } from 'react-native'; 
-import { Header, Slider } from 'react-native-elements'; 
-import { Back } from '../../components/Back'; 
-import styles from './styles'; 
-import firebase from 'firebase'; 
-import { random } from '../../services'; 
-import { QuestionTypeOne } from '../../components/Questions/QuestionTypeOne'; 
-import { QuestionTypeTwo } from '../../components/Questions/QuestionTypeTwo'; 
-import { QuestionTypeThree } from '../../components/Questions/QuestionTypeThree'; 
-import { QuestionTypeFour } from '../../components/Questions/QuestionTypeFour'; 
+import React, { useState, useEffect } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import { Header, Slider } from 'react-native-elements';
+import { Back } from '../../components/Back';
+import styles from './styles';
+import firebase from 'firebase';
+import { random } from '../../services';
+import { QuestionTypeOne } from '../../components/Questions/QuestionTypeOne';
+import { QuestionTypeTwo } from '../../components/Questions/QuestionTypeTwo';
+import { QuestionTypeThree } from '../../components/Questions/QuestionTypeThree';
+import { QuestionTypeFour } from '../../components/Questions/QuestionTypeFour';
 import { AnswerTypeOne } from '../../components/Answers/AnswerTypeOne';
-import { AnswerTypeTwo } from '../../components/Answers/AnswerTypeTwo'; 
-import { AnswerTypeThree } from '../../components/Answers/AnswerTypeThree'; 
-import { AnswerTypeFour } from '../../components/Answers/AnswerTypeFour'; 
-import { AskQuestionNumber } from '../../components/AskQuestionNumber'; 
-import { PracticeTestResult } from '../../components/PracticeTestResult'; 
+import { AnswerTypeTwo } from '../../components/Answers/AnswerTypeTwo';
+import { AnswerTypeThree } from '../../components/Answers/AnswerTypeThree';
+import { AnswerTypeFour } from '../../components/Answers/AnswerTypeFour';
+import { AskQuestionNumber } from '../../components/AskQuestionNumber';
+import { PracticeTestResult } from '../../components/PracticeTestResult';
 import { Activity } from '../Utils/activity';
-
-const Practice = (props: {route?: any; navigation?: any}) => {
-  const { route, navigation } = props;  
-  const lessonInfo = route.params; 
-  const [amountOfQuestion, setAmountOfQ] = useState(0); 
-  const [contentOfQuestion, setContentOfQ] = useState({ type: '', status: 'loading' }); 
-  const [contentOfAnswer, setContentOfA] = useState({ type: '', status: 'loading' }); 
-  const [questionNumber, setQuestionNumber] = useState(0);  
-  const [nextQuestion, setNextQuestion] = useState(false); 
-  const [count, setCount] = useState(0); 
+import * as Progress from 'react-native-progress';
+const WIDTH = Dimensions.get('window').width;
+const Practice = (props: { route?: any; navigation?: any }) => {
+  const { route, navigation } = props;
+  const lessonInfo = route.params;
+  const [amountOfQuestion, setAmountOfQ] = useState(0);
+  const [contentOfQuestion, setContentOfQ] = useState({ type: '', status: 'loading' });
+  const [contentOfAnswer, setContentOfA] = useState({ type: '', status: 'loading' });
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [nextQuestion, setNextQuestion] = useState(false);
+  const [count, setCount] = useState(0);
   const database = firebase.database();
-  
+  console.log(count);
   useEffect(() => {
-    setAmountOfQ(0); 
-    setCount(0); 
-    setContentOfQ({type: '', status: 'loading'}); 
-    setContentOfA({type: '', status: 'loading'}); 
-    if (nextQuestion){
-      setNextQuestion(false); 
+    setAmountOfQ(0);
+    setCount(0);
+    setContentOfQ({ type: '', status: 'loading' });
+    setContentOfA({ type: '', status: 'loading' });
+    if (nextQuestion) {
+      setNextQuestion(false);
     }
   }, [lessonInfo])
 
@@ -47,7 +48,7 @@ const Practice = (props: {route?: any; navigation?: any}) => {
 
   useEffect(() => {
     if (nextQuestion) {
-      let c = count + 1; 
+      let c = count + 1;
       setCount(c);
       setQuestionNumber(random(0, 19));
       // setQuestionNumber(0);
@@ -55,28 +56,28 @@ const Practice = (props: {route?: any; navigation?: any}) => {
   }, [nextQuestion])
 
   useEffect(() => {
-    const question = database.ref('/topic_detail/' + 
-    lessonInfo.topicName + '/test_bank/' + lessonInfo.lessonName + 
-    '/questions/' + questionNumber); 
-    question.on('value', function(snapshot: any){
+    const question = database.ref('/topic_detail/' +
+      lessonInfo.topicName + '/test_bank/' + lessonInfo.lessonName +
+      '/questions/' + questionNumber);
+    question.on('value', function (snapshot: any) {
       if (snapshot.val()) {
-        setContentOfQ(snapshot.val()); 
+        setContentOfQ(snapshot.val());
       } else {
-        setContentOfQ({type: '', status: 'null'}); 
+        setContentOfQ({ type: '', status: 'null' });
       }
-    }); 
-    const answer = database.ref('/topic_detail/' + 
-    lessonInfo.topicName + '/test_bank/' + lessonInfo.lessonName + 
-    '/answers/' + questionNumber); 
-    answer.on('value', function(snapshot: any){ 
+    });
+    const answer = database.ref('/topic_detail/' +
+      lessonInfo.topicName + '/test_bank/' + lessonInfo.lessonName +
+      '/answers/' + questionNumber);
+    answer.on('value', function (snapshot: any) {
       if (snapshot.val()) {
-        setContentOfA(snapshot.val()); 
+        setContentOfA(snapshot.val());
       } else {
         setContentOfA({ type: '', status: 'null' })
       }
     })
-    if (nextQuestion){
-      setNextQuestion(false); 
+    if (nextQuestion) {
+      setNextQuestion(false);
     }
   }, [questionNumber])
 
@@ -84,12 +85,12 @@ const Practice = (props: {route?: any; navigation?: any}) => {
     return (
       <AskQuestionNumber setAmountOfQ={setAmountOfQ} />
     )
-  } 
+  }
   else {
     if (count < amountOfQuestion) {
       if (contentOfQuestion.status == 'loading' || contentOfAnswer.status == 'loading') {
-        return (<Activity/>)
-      } 
+        return (<Activity />)
+      }
       else if (contentOfQuestion.status == 'null' || contentOfAnswer.status == 'null') {
         return (
           <View>
@@ -105,68 +106,68 @@ const Practice = (props: {route?: any; navigation?: any}) => {
         )
       }
       else {
-        let ContentQuestion; 
+        let ContentQuestion;
         if (contentOfQuestion) {
-          if (contentOfQuestion.type == '1'){
-            ContentQuestion = <QuestionTypeOne 
-              content={contentOfQuestion} 
+          if (contentOfQuestion.type == '1') {
+            ContentQuestion = <QuestionTypeOne
+              content={contentOfQuestion}
               id={count}
             />
           }
           else if (contentOfQuestion.type == '2') {
-            ContentQuestion = <QuestionTypeTwo 
+            ContentQuestion = <QuestionTypeTwo
               content={contentOfQuestion}
               id={count}
             />
           }
           else if (contentOfQuestion.type == '3') {
-            ContentQuestion = <QuestionTypeThree 
+            ContentQuestion = <QuestionTypeThree
               content={contentOfQuestion}
               id={count}
             />
           }
           else if (contentOfQuestion.type == '4') {
-            ContentQuestion = <QuestionTypeFour 
+            ContentQuestion = <QuestionTypeFour
               content={contentOfQuestion}
               id={count}
             />
-          } 
+          }
         } else {
           ContentQuestion = (<View>
             <Text>Waiting data ...</Text>
           </View>)
         }
-    
-        let ContentAnswer; 
-  
-        if(contentOfAnswer) {
-          if (contentOfAnswer.type == '1'){
-            ContentAnswer = <AnswerTypeOne 
-              content={contentOfAnswer} 
+
+        let ContentAnswer;
+
+        if (contentOfAnswer) {
+          if (contentOfAnswer.type == '1') {
+            ContentAnswer = <AnswerTypeOne
+              content={contentOfAnswer}
               lessonInfo={lessonInfo}
               setNextQuestion={setNextQuestion}
               id={count}
             />
           }
           else if (contentOfAnswer.type == '2') {
-            ContentAnswer = <AnswerTypeTwo 
-              content={contentOfAnswer} 
+            ContentAnswer = <AnswerTypeTwo
+              content={contentOfAnswer}
               lessonInfo={lessonInfo}
               setNextQuestion={setNextQuestion}
               id={count}
             />
           }
           else if (contentOfAnswer.type == '3') {
-            ContentAnswer = <AnswerTypeThree 
-              content={contentOfAnswer} 
+            ContentAnswer = <AnswerTypeThree
+              content={contentOfAnswer}
               lessonInfo={lessonInfo}
               setNextQuestion={setNextQuestion}
               id={count}
             />
           }
           else if (contentOfAnswer.type == '4') {
-            ContentAnswer = <AnswerTypeFour 
-              content={contentOfAnswer} 
+            ContentAnswer = <AnswerTypeFour
+              content={contentOfAnswer}
               lessonInfo={lessonInfo}
               setNextQuestion={setNextQuestion}
               id={count}
@@ -178,7 +179,7 @@ const Practice = (props: {route?: any; navigation?: any}) => {
           </View>)
         }
         return (
-          <View>
+          <View style={styles.container}>
             <Header containerStyle={styles.headerContainer}
               leftComponent={
                 <Back
@@ -186,13 +187,17 @@ const Practice = (props: {route?: any; navigation?: any}) => {
                 />}
               centerComponent={{ text: lessonInfo.lessonName, style: styles.headerTitle }}
             />
+            <View style={styles.count}>
+              <Text style={{ paddingTop: 10, fontSize: 16, color: '#ff5e00' }}>{count}/{amountOfQuestion}</Text>
+            </View>
+
             <View style={styles.slider}>
-              <Text>{count}/{amountOfQuestion}</Text>
-              <Slider 
+              {/* <Slider
                 disabled={true}
                 value={count}
                 maximumValue={amountOfQuestion}
-              />
+              /> */}
+              <Progress.Bar progress={(count/amountOfQuestion)} width={WIDTH-15} color="#ff5e00" height={8}/>
             </View>
             <View>
               {ContentQuestion}
@@ -213,12 +218,13 @@ const Practice = (props: {route?: any; navigation?: any}) => {
             centerComponent={{ text: lessonInfo.lessonName, style: styles.headerTitle }}
           />
           <View>
-            <PracticeTestResult 
+            <PracticeTestResult
               content={{
-                topicName: lessonInfo.topicName, 
-                lessonName: lessonInfo.lessonName, 
-                correct: 5, 
-                incorrect: 10}}
+                topicName: lessonInfo.topicName,
+                lessonName: lessonInfo.lessonName,
+                correct: 5,
+                incorrect: 10
+              }}
               navigation={navigation}
             />
           </View>
--- 8/Eng/src/android/screens/PracticeTestScreen/styles.tsx
+++ 8/Eng/src/android/screens/PracticeTestScreen/styles.tsx
@@ -1,25 +1,30 @@
-import { StyleSheet } from 'react-native'; 
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   headerContainer: {
     paddingTop: 0,
     height: 60,
-    backgroundColor:'#ff5e00'
-  }, 
+    backgroundColor: '#ff5e00'
+  },
   headerTitle: {
     color: '#fff',
     fontWeight: '700',
     fontSize: 20,
-  }, 
+  },
   slider: {
-    alignItems: 'stretch', 
-    justifyContent: 'center', 
-    paddingLeft: 10, 
-    paddingRight: 10, 
-  }, 
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
   score: {
-    
+
+  },
+  container: {
+  },
+  count: {
+    alignItems: 'center',
   }
-}); 
+});
 
 export default styles; 
