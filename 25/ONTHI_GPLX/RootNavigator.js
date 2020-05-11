1846a803c3526da52a7d67efb7eb2682aa4e0cdd
ngocAnh1999 <anhshmily2512@gmail.com>
2020-05-12 00:45:59 +0700

    Kết nối với redux

--- 25/ONTHI_GPLX/RootNavigator.js
+++ 25/ONTHI_GPLX/RootNavigator.js
@@ -3,13 +3,13 @@ import React, {Suspense} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Spinner, } from 'native-base';
-
+// import InitExam from './screens/initExam';
 const Home = React.lazy(() => import('./screens/Home'));
 
 const ListComponent= React.lazy(() => import('./screens/ListComponent'));
 const ListTrafficSigns= React.lazy(() => import('./screens/ListTrafficSigns'));
 const ListTopicExam= React.lazy(() => import('./screens/ListTopicExam'));
-const initExam = React.lazy(()=> import('./screens/initExam'));
+const InitExam = React.lazy(()=> import('./screens/initExam'));
 const TipRemember = React.lazy(()=> import('./screens/TipRemember'));
 
 const Stack = createStackNavigator();
@@ -23,7 +23,7 @@ const AuthNavigator = () => {
                 <Stack.Screen name={"ListComponent"} component={ListComponent}/>
                 <Stack.Screen name= {"ListTrafficSigns"} component={ListTrafficSigns} />
                 <Stack.Screen name= {"ListTopicExam"} component={ListTopicExam} />
-                <Stack.Screen name={"initExam"} component={initExam} />
+                <Stack.Screen name={"initExam"} component={InitExam} />
                 <Stack.Screen name={"TipRemember"} component={TipRemember} />
             </Stack.Navigator>
             </Suspense>
