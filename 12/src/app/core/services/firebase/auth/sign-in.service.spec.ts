fa6605bd05278f8e482fb54ad1666daa66ff4f20
duong755 <ngo.quang.duong.1100@gmail.com>
2020-05-11 18:12:00 +0700

    Prevent karma randomness; Add specs to sign in and sign out service

--- 12/src/app/core/services/firebase/auth/sign-in.service.spec.ts
+++ 12/src/app/core/services/firebase/auth/sign-in.service.spec.ts
@@ -20,4 +20,21 @@ describe('SignInService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should failed', async () => {
+    const email = '17020191@vnu.edu.vn';
+    const password = '12345678';
+    try {
+      await service.signInWithEmailAndPassword(email, password).toPromise();
+    } catch (err) {
+      expect(err).toBeTruthy();
+    }
+  });
+
+  it('should success', async () => {
+    const email = '17020191@vnu.edu.vn';
+    const password = '17020191';
+    const user = await service.signInWithEmailAndPassword(email, password).toPromise();
+    expect(user).toBeTruthy();
+  });
 });
