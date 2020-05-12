7827b382cdff40945a9d806f2db255eaa629bc8e
huyengocao <43350545+huyengocao@users.noreply.github.com>
2020-05-12 15:55:29 +0700

    Update Test File

---
+++ 25/ONTHI_GPLX/__tests__/initExam-test.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import InitExam from '../screens/initExam';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import InitExam from '../screens/initExam';
+
+it('renders correctly', () => {
+  renderer.create(<InitExam />);
+});
