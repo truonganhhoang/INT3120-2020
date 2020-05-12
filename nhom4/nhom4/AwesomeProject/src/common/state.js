fb3a3ebafd3a59e5ca4eb61020db2e75073ecab2
luong <duongvanluong519@gmail.com>
2020-05-12 09:52:32 +0700

    Remove duplicated directory

--- nhom4/nhom4/AwesomeProject/src/common/state.js
+++
@@ -1,9 +0,0 @@
-import React, { useState,useEffect } from 'react';
-import{
-  Text,View
-} from 'react-native';
-
-export function setStateArray (key, value, id) {
-    const nextState = state.map(a => a.id === id ? { ...a, [key]: value } : a);
-    setState(nextState);
-   };
