48a2a5016881719e2dea89f8651f5989b8ae596a
vantu <nguyenvantuhym@gmail.com>
2020-05-16 11:42:50 +0700

    login gg fix bug save data user

--- 68/components/Login.js
+++ 68/components/Login.js
@@ -1,43 +1,34 @@
+/* eslint-disable no-mixed-operators */
 import React from 'react';
-import {View,Text, Button, StyleSheet, Image,  Dimensions}from 'react-native';
-import kanji from '../assets/kanji.png'
+import {
+  View, Text, StyleSheet, Image, Dimensions
+} from 'react-native';
+import kanji from '../assets/kanji.png';
 
 const deviceWidth = Dimensions.get('window').width;
 const screen = (percent) => deviceWidth * percent / 100;
 export default function Login(props) {
-    const {onPress} = props;
-    return (
-        <View>
-          <View style={styles.title}>
-            <Text style={styles.titleText}>
-                Chào mừng bạn đến với 
-            </Text>
-            <Text style={styles.titleText}>
-                Kanji45 
-            </Text>
-          </View>
-          <View style={styles.logo}>
-          <Image
-            style={styles.logoKanji}
-            source={kanji}
-            />
-          </View>
-         <View style={styles.button} >
-         <Button onPress={onPress}
-            title="Tiếp tục đăng nhập bằng Facebook"
-            color="#4267b2"
-            />
-         </View>
-         <View style={styles.button} >
-         <Button onPress={onPress}
-            title="Tiếp tục đăng nhập bằng Google"
-            color="#e73232"
-            />
-         </View>
-         
-         
-        </View>
-    )
+  return (
+    <View>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>
+          Chào mừng bạn đến với
+        </Text>
+        <Text style={styles.titleText}>
+          Kanji45
+        </Text>
+      </View>
+      <View style={styles.logo}>
+        <Image
+          style={styles.logoKanji}
+          source={kanji}
+        />
+      </View>
+      {
+        props.children
+      }
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   title: {
@@ -48,19 +39,13 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#006265',
     textAlign: 'center',
-    fontWeight: "900"
-  }, 
+    fontWeight: '900'
+  },
   logo: {
-    marginLeft:screen(20)
+    marginLeft: screen(20)
   },
   logoKanji: {
-      width: screen(60),
-      height: screen(60)
+    width: screen(60),
+    height: screen(60)
   },
-  button: {
-    marginTop: screen(5),
-    width: screen(80),
-    marginLeft: screen(10),
-  }
-
 });
--- 68/screens/LoginScreen.js
+++ 68/screens/LoginScreen.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-mixed-operators */
 import React from 'react';
 import * as Google from 'expo-google-app-auth';
 import * as Facebook from 'expo-facebook';
 import firebase from 'firebase';
-
+import {
+  StyleSheet, View, Button, Dimensions
+} from 'react-native';
 import Login from '../components/Login';
+
+const deviceWidth = Dimensions.get('window').width;
+const screen = (percent) => deviceWidth * percent / 100;
 
 export default class LoginScreen extends React.Component {
     static navigationOptions = () => ({
@@ -18,6 +25,51 @@ export default class LoginScreen extends React.Component {
       },
     });
 
+    onSignIn = (googleUser) => {
+      console.log(googleUser);
+      // console.log('Google Auth Response', googleUser);
+      // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+      const unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!this.isUserEqual(googleUser, firebaseUser)) {
+          // Build Firebase credential with the Google ID token.
+          console.log(googleUser.getAuthResponse().id_token);
+          const credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.idToken,
+            googleUser.accessToken,
+          );
+          // Sign in with credential from the Google user.
+          firebase.auth().signInWithCredential(credential).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const { email } = error;
+            // The firebase.auth.AuthCredential type that was used.
+            const { credential } = error;
+            // ...
+          });
+        } else {
+          console.log('User already signed-in Firebase.');
+        }
+      });
+    }
+
+    isUserEqual = (googleUser, firebaseUser) => {
+      if (firebaseUser) {
+        const { providerData } = firebaseUser;
+        for (let i = 0; i < providerData.length; i++) {
+          if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID
+              && providerData[i].uid === googleUser.getBasicProfile().getId()) {
+            // We don't need to reauth the Firebase connection.
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
     signInWithGoogleAsync = async () => {
       try {
         const result = await Google.logInAsync({
@@ -26,9 +78,17 @@ export default class LoginScreen extends React.Component {
           scopes: ['profile'],
         });
         if (result.type === 'success') {
-          console.log(result);
-          return result.accessToken;
+          console.log('asdasd');
+          const credential = firebase.auth.GoogleAuthProvider.credential(
+            result.idToken,
+            result.accessToken,
+          );
+          // Sign in with credential from the Google user.
+          firebase.auth().signInWithCredential(credential);
+          // console.log(result);
+          // return result.accessToken;
         }
+        console.log('asdasd');
         return { cancelled: true };
       } catch (e) {
         return { error: true };
@@ -57,9 +117,46 @@ export default class LoginScreen extends React.Component {
       }
     }
 
+    logout = () => {
+      firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+    }
+
     render() {
       return (
-        <Login onPress={() => this.logInFacebook()} />
+        <Login>
+          <View style={styles.button}>
+            <Button
+              onPress={this.logInFacebook}
+              title="Tiếp tục đăng nhập bằng Facebook"
+              color="#4267b2"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              onPress={this.signInWithGoogleAsync}
+              title="Tiếp tục đăng nhập bằng Google"
+              color="#e73232"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              onPress={this.logout}
+              title=" logout"
+              color="#e73232"
+            />
+          </View>
+        </Login>
       );
     }
 }
+const styles = StyleSheet.create({
+  button: {
+    marginTop: screen(5),
+    width: screen(80),
+    marginLeft: screen(10),
+  }
+});
