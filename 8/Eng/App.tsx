bddcf8e7b9c9820a23247db6e149928913cf1e69
Quang Nguyễn <adquang199x@gmail.com>
2020-05-19 22:17:28 +0700

    re init project

--- 8/Eng/App.tsx
+++ 8/Eng/App.tsx
@@ -11,23 +11,25 @@
 import * as React from 'react';
 import { MyApp } from './src/android/navigation'; 
 import firebase from 'firebase'; 
-
-var firebaseConfig = { 
-  apiKey: "AIzaSyCIPo4i1f8qzNtVLEVVPh34ubCs6pv3UUs",
-  authDomain: "english-uet-team.firebaseapp.com",
-  databaseURL: "https://english-uet-team.firebaseio.com",
-  projectId: "english-uet-team",
-  storageBucket: "english-uet-team.appspot.com",
-  messagingSenderId: "151102314176",
-  appId: "1:151102314176:web:866bcec21e8f77a205bf25",
-  measurementId: "G-G550WJ0Q0E"
-};
+import firebaseConfig from './src/android/constants/firebaseConfig'; 
+import { getDataFromStorage, storeDataToStorage } from './src/android/services'; 
+import { AsyncStorage } from 'react-native'; 
 
 const App = () => {
   if (!firebase.apps.length){
     firebase.initializeApp(firebaseConfig); 
   }
-  
+  const check = async () => {
+    let lessons = await getDataFromStorage('favoriteLessons'); 
+    if (lessons == null) {
+      storeDataToStorage('favoriteLessons', JSON.stringify({}))
+    }
+    let words = await getDataFromStorage('favoriteWords'); 
+    if (words == null) {
+      storeDataToStorage('favoriteWords', JSON.stringify({}))
+    }
+  }
+  check(); 
   return (
     <MyApp />
   )
