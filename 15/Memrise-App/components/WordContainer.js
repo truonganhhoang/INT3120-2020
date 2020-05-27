fe3ee80d8352a0d2d77782bec4544ed392281b66
vantuhym <nguyenvantuhym@gmail.com>
2020-05-27 22:20:12 +0700

    sua thunder image

--- 15/Memrise-App/components/WordContainer.js
+++ 15/Memrise-App/components/WordContainer.js
@@ -1,29 +1,52 @@
-import React from "react";
-import { Text, Image, View, StyleSheet } from "react-native";
+import React, {useEffect, useState} from "react";
+import { Text, Image, View, StyleSheet, Dimensions , TouchableOpacity,Touchable } from "react-native";
 
 import TreeImages from "../TreeImages";
 import planet from "../assets/planet.png";
 import thunder from "../assets/thunder.png";
 import wateringCan from "../assets/watering-can.png";
 
+const deviceWidth = Dimensions.get("window").width;
+const screen = (percent) => (percent * deviceWidth) / 100;
+const blur =0.1;
 export default function WordContainer(props) {
-  const { word, mean,level } = props.objWord;
-  const hideMean =props.hideMean;
+  const { word, mean, level } = props.objWord;
+  const hideMean = props.hideMean;
+
+  const [blurThunder, setBlurThunder] = useState(1);
+  useEffect(() => {
+    //call api
+  }, [blurThunder]);
+
+  function handlOnPress(){
+    
+    if( blurThunder ===1 ){
+      setBlurThunder(blur)
+    }else{
+      setBlurThunder(1)
+    }
+
+  }
+
   return (
     <View style={styles.WordContainer}>
       <View style={styles.WordInfor}>
         <Text style={styles.Word}>{word}</Text>
 
-        {hideMean||<Text style={styles.Mean}>{mean}</Text>}
-
+        {hideMean || <Text style={styles.Mean}>{mean}</Text>}
       </View>
-
+    
       <View>
         <Image style={styles.WordImage} source={TreeImages[level]} />
 
         <View styles={styles.ThunderWater}>
-          {/* <Image style={styles.ThunderImage} source={thunder} /> */}
-          {/* <Image  style={styles.WaterImage}  source ={water}/> */}
+        <TouchableOpacity
+            
+            onPress={handlOnPress}
+        >
+             <Image style={ [styles.ThunderImage,{opacity: blurThunder }] } source={thunder} />
+        </TouchableOpacity>
+         
         </View>
       </View>
     </View>
@@ -34,8 +57,10 @@ const styles = StyleSheet.create({
   WordContainer: {
     flexDirection: "row",
     alignItems: "flex-start",
-    padding: 30,
-    marginBottom: 30,
+    paddingTop: screen(5),
+    paddingLeft: screen(7),
+    paddingBottom: screen(4.5),
+    marginBottom:screen(5),
   },
   WordInfor: {
     flexDirection: "column",
@@ -43,38 +68,37 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
   },
   ThunderWater: {
-    flexDirection: "row",
+    position: "absolute",
+    right: screen(2),
+    bottom: screen(2),
   },
   WordImage: {
-    width: 65,
-    height: 65,
+    width: screen(25),
+    height: screen(25),
     backgroundColor: "green",
-    // borderStyle: "solid",
     borderWidth: 0.5,
     borderRadius: 80,
+    marginRight: screen(7),
   },
   ThunderImage: {
-    marginTop: 20,
-    width: 35,
-    height: 35,
-    opacity: 0.4,
+    position: "absolute",
+    right: screen(14),
+    marginVertical: screen(3),
+    width: screen(10),
+    height: screen(10),
+    
   },
-  WaterImage: {
-    marginTop: 20,
-    width: 35,
-    height: 35,
-    marginLeft: "auto",
-  },
+
   Word: {
     textTransform: "lowercase",
     fontWeight: "700",
-    fontSize: 50,
-    marginBottom: 20,
+    fontSize: screen(10),
+    paddingTop: screen(3),
   },
   Mean: {
     textTransform: "lowercase",
     fontWeight: "500",
     color: "#030",
-    fontSize: 30,
+    fontSize: screen(9),
   },
 });
